49e2982a50ffb503f99d3af32c363ae2c8a042f0
PCJRibeiro <pcjr2003@gmail.com>
2024-02-02 20:15:36 -0300

    Projeto completo

---
+++ Estacionamento/Estacionamento/Entites/ClasseEstacionamento.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estacionamento.Entites
+{
+    internal class ClasseEstacionamento
+    {
+        public int Hora {  get; set; }
+        public int PrecoInicial { get; set; }
+        public int PrecoHora { get; set; }
+
+        public List<string> Carros = new List<string>();
+
+        public ClasseEstacionamento() { }
+        public ClasseEstacionamento(int precoInicial,int precoHora)
+        {
+             PrecoInicial = precoInicial;
+             PrecoHora = precoHora;
+        }
+
+        public void AdicionarCarro(string placa)
+        {           
+            Carros.Add(placa);
+        }
+        public string RemoverCarro(string placa,int hora)
+        {           
+            Hora = hora;
+            Carros.Remove(placa);
+            return "O valor a pagar é de R$"+(PrecoInicial +PrecoHora*Hora);
+        }
+        public void ListarCarros()
+        {
+            foreach(string carro in Carros)
+            {
+                Console.WriteLine(carro);
+            }
+        }
+    }
+}
---
+++ Estacionamento/Estacionamento/Program.cs
@@ -0,0 +1,59 @@
+﻿using Estacionamento.Entites;
+
+
+Console.WriteLine("Digite o preço inicial:");
+int PrecoInicial = int.Parse(Console.ReadLine());
+Console.WriteLine("Agora digite o preço por hora:");
+int PrecoHora = int.Parse(Console.ReadLine());
+int escolha = 0;
+ClasseEstacionamento estacionamento1 = new ClasseEstacionamento();
+estacionamento1.PrecoInicial = PrecoInicial;
+estacionamento1.PrecoHora = PrecoHora;
+
+while (escolha != 4) 
+
+    {
+    Console.WriteLine("Digite a sua opção: ");
+
+    Console.WriteLine("1 - Cadastrar veículo");
+    Console.WriteLine("2 - Remover veículo");
+    Console.WriteLine("3 - Listar veículo");
+    Console.WriteLine("4 - Encerrar");
+    escolha = int.Parse(Console.ReadLine());
+    if (escolha == 1)
+    {
+        Console.WriteLine("Digite a placa do veículo para estacionar:");
+        String Placa = Console.ReadLine();
+        Console.WriteLine(estacionamento1.AdicionarCarro(Placa));      
+        Console.WriteLine("Pressione uma tecla para continuar");
+        Console.ReadLine();
+        Console.Clear();
+    }
+    else if (escolha == 2)
+    {
+        Console.Write("Digite a placa do veículo para retirar: ");
+        String Placa = Console.ReadLine();
+        Console.Write("Digite a quantidade de horas no estacionamento: ");
+        int hora = int .Parse(Console.ReadLine());
+        Console.WriteLine(estacionamento1.RemoverCarro(Placa, hora));
+        Console.WriteLine("Pressione uma tecla para continuar");
+        Console.ReadLine();
+        Console.Clear();
+    }
+    else if (escolha == 3)
+    {
+        Console.WriteLine("Segue os carros estacionados:");     
+        estacionamento1.ListarCarros();
+        Console.WriteLine("Pressione uma tecla para continuar");
+        Console.ReadLine();
+        Console.Clear();
+    }
+    else if (escolha == 4)
+    {
+        Console.WriteLine("Fechando o programa!");
+    }
+    else
+    {
+        Console.WriteLine("Insira uma opção válida!");
+    }
+    }
